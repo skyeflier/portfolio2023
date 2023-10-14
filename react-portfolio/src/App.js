4499fd2deb942bda1e5c30c2ece42b4008d500e0
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-10-14 13:33:48 -0500

    adding animation to letters on home page

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
+import Home from './components/Home/Logo'
 // import About from './components/About'
 // import Contact from './components/Contact'
 import Layout from './components/Layout'

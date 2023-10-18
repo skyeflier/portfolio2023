f7a8e06468bc4c3bbc1bae0ebac6c1d9258b038e
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-10-18 17:46:49 -0500

    Resolved technical issues with the body copy not showing up on the live link

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,22 +1,20 @@
-import { Route, Routes } from 'react-router-dom'
+import './App.scss';
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
-// import About from './components/About'
-// import Contact from './components/Contact'
-// import Portfolio from './components/Portfolio'
-// import Dashboard from './components/Dashboard'
-import './App.scss'
+import About from './components/About'
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
     </>
   )
 }
 
-export default App
+export default App;
